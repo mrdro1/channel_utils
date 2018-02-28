072ee5f4287a4be0dc3858c451130fa14e9314b0
Mikle <naza09@yandex.ru>
2018-02-28 18:55:31 +0300

    a

--- utils.py
+++ utils.py
@@ -5,5 +5,10 @@ def read_token():
         token = f.readline()
     return token
 
-a = read_token()
-print(a)
+def read_login_pwd():
+    with open('login_pwd.ctl', 'r') as f:
+        login, pwd = f.readline().split(':')
+    return login, pwd
+
+#a = read_login_pwd()
+#print(a)
